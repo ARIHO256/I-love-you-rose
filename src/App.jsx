15ab0f4178c8ef1ce87afcdf48a72fa99096d9bf
Ariho Nowamaani <141637552+ARIHO256@users.noreply.github.com>
2025-06-11 05:06:27 +0300

    EDIT

--- src/App.jsx
+++ src/App.jsx
@@ -393,7 +393,7 @@ const Footer = () => {
   return (
     <a
       className="fixed bottom-2 right-2 backdrop-blur-md opacity-80 hover:opacity-95 border p-1 rounded border-rose-300"
-      href="https://github.com/ARIHO256aini07"
+      href="https://github.com/ARIHO256"
       target="_blank"
       rel="noopener noreferrer"
     >
